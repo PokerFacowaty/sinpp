94d5d41f3c504998eb2e62a413ba59950882987a
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-06-26 20:37:56 +0200

    Added tests for roles

--- schedule/tests/test_role.py
+++ schedule/tests/test_role.py
@@ -1,10 +1,39 @@
 from django.test import TestCase
-from schedule.models import Role
-from datetime import datetime, timedelta
+from django.utils import timezone
+from schedule.models import Role, Event
+from datetime import timedelta
 
 
 class RoleTestCase(TestCase):
-    # TODO:
-    # - Check for default visibility
-    # - Check hour based default
-    pass
+
+    def setUp(self):
+        event_start_date = timezone.now()
+        event_end_date = timezone.now() + timedelta(days=1)
+        gtam = Event.objects.create(NAME="GTAMarathon 2027",
+                                    SHORT_TITLE="GTAM27",
+                                    START_DATE_TIME=event_start_date,
+                                    END_DATE_TIME=event_end_date)
+        Role.objects.create(NAME="Tech", EVENT=gtam)
+        Role.objects.create(NAME="Fundraising", EVENT=gtam, VISIBILITY="PU",
+                            HOUR_BASED=True)
+
+    def test_default_values_visibility(self):
+        role = Role.objects.get(NAME="Tech")
+        self.assertEqual(role.VISIBILITY, "RL")
+
+    def test_default_values_hour_based(self):
+        role = Role.objects.get(NAME="Tech")
+        self.assertFalse(role.HOUR_BASED)
+
+    def test_non_default_values_visibility(self):
+        role = Role.objects.get(NAME="Fundraising")
+        self.assertEqual(role.VISIBILITY, "PU")
+
+    def test_non_default_values_hour_based(self):
+        role = Role.objects.get(NAME="Fundraising")
+        self.assertTrue(role.HOUR_BASED)
+
+    def test_proper_event(self):
+        event = Event.objects.get(NAME="GTAMarathon 2027")
+        role = Role.objects.get(NAME="Tech")
+        self.assertEqual(role.EVENT, event)
