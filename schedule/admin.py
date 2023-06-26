3dadfd0342bb4ca1a3ccc0882b9e742025a32e2e
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-06-26 19:59:25 +0200

    Registered models to admin

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Event, Speedrun, Shift, Person, AvailabilityBlock, Role
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Speedrun)
+admin.site.register(Shift)
+admin.site.register(Person)
+admin.site.register(AvailabilityBlock)
+admin.site.register(Role)
