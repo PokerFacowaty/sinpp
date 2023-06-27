2da5015e166226b2d5cf3125915da6c13fbf4d1f
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-06-28 00:18:22 +0200

    Updated the settings for PostgresSQL integration

--- sinpp/settings.py
+++ sinpp/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'sinpp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ["DB_TABLE"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': os.environ["DB_HOST"],
+        'PORT': os.environ["DB_PORT"],
     }
 }
 
