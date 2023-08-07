b1f49016f6c11e3a4729e5ed14dc0e499e48ed6f
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-08-08 01:03:56 +0200

    Added a draft of Oengus csv schedule parsing

---
+++ schedule/parse_schedule_csv.py
@@ -0,0 +1,35 @@
+import csv
+from pathlib import Path
+from models import Event, Speedrun, Intermission
+from datetime import datetime as dt
+from datetime import time
+
+
+def parse_oengus(filepath: str, event: Event):
+    with open(Path(filepath)) as cf:
+        rdr = csv.DictReader(cf)
+
+        for row in rdr:
+            # NOTE: Oengus estimates and setup times are not ISO 8601
+            # because of no trailing zeros on hours, so that's why I'm parsing
+            # them manually instead of using time.fromisoformat
+
+            run_start = dt.fromisoformat(row["time"])
+            estimate = [int(x) for x in row["estimate"].split(":")]
+            estimate = time(hour=estimate[0],
+                            minute=estimate[1],
+                            second=estimate[2])
+            inter_dur = [int(y) for y in row["setup_time"].split(":")]
+            inter_dur = time(hour=inter_dur[0],
+                             minute=inter_dur[1],
+                             second=inter_dur[2])
+
+            Speedrun(EVENT=event,
+                     GAME=row["game"],
+                     CATEGORY=row["category"],
+                     START_TIME=run_start,
+                     ESTIMATE=estimate)
+
+            Intermission(EVENT=event,
+                         START_TIME=run_start + estimate,
+                         DURATION=inter_dur)
