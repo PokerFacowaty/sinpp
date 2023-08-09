88338de98d6986736385a589864fae023b38d31e
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-08-09 02:07:25 +0200

    Added a simple login page

--- sinpp/urls.py
+++ sinpp/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('schedule.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
