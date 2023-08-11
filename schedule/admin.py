8df7d12391c271cc2c6ce673ecdcf219b49791eb
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-08-11 04:34:09 +0200

    Registered the Intermission model on the admin page

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Event, Speedrun, Shift, Person, AvailabilityBlock, Role
+from .models import (Event, Speedrun, Shift, Person, AvailabilityBlock, Role,
+                     Intermission)
 
 admin.site.register(Event)
 admin.site.register(Speedrun)
@@ -7,3 +8,4 @@ admin.site.register(Shift)
 admin.site.register(Person)
 admin.site.register(AvailabilityBlock)
 admin.site.register(Role)
+admin.site.register(Intermission)
