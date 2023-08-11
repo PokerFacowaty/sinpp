d5c7713167fedacfde8e21d870f9ceb501c62335
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-08-11 06:07:43 +0200

    Added a simple profile page

--- sinpp/urls.py
+++ sinpp/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('', include('schedule.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/profile', views.user_profile),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
