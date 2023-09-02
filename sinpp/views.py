bed8c973739d65fec6a75fefbddcbbeccb6f923e
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-09-02 14:44:22 +0200

    Added simple listing of events and rooms to users' profiles

--- sinpp/views.py
+++ sinpp/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from schedule.views import index
+from schedule.models import Event, Room
+from django.db.models import Q
 
 
 @login_required
 def user_profile(request):
-    return render(request, 'registration/profile.html')
+    usr = request.user
+    groups = usr.groups.all()
+    events_rooms = {x: [] for x in Event.objects.filter(Q(STAFF__in=groups))}
+    for ev in events_rooms.keys():
+        events_rooms[ev] = [x for x in Room.objects.filter(EVENT=ev)]
+    print(events_rooms)
+    return render(request, 'registration/profile.html',
+                  {'events_rooms': events_rooms})
 
 
 def register_account(request):
