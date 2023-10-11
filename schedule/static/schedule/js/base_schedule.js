f84f83a36e1729b173bbb2c61a8ba23dc0d54038
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-10-11 16:22:13 +0000

    Elements changing size and position when loading are not initially invisible

--- schedule/static/schedule/js/base_schedule.js
+++ schedule/static/schedule/js/base_schedule.js
@@ -2,9 +2,10 @@ function main(){
     const blocks = document.getElementsByClassName("block");
     for (const bl of blocks){
         bl.style.position = "absolute";
-        bl.style.top = `${Number(bl.getAttribute("data-start")) * 2 + 1}px`
-        let length = Number(bl.getAttribute("data-length"))
-        bl.style.height = `${length * 2}px`
+        bl.style.top = `${Number(bl.getAttribute("data-start")) * 2 + 1}px`;
+        let length = Number(bl.getAttribute("data-length"));
+        bl.style.height = `${length * 2}px`;
+        bl.style.visibility = "visible";
     }
 }
 
