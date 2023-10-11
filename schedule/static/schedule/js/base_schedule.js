798cc74ead01a5604859d5a11bf0980fe2ebaa7f
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-10-11 19:02:03 +0000

    Added basic dialogs to the schedule interface

--- schedule/static/schedule/js/base_schedule.js
+++ schedule/static/schedule/js/base_schedule.js
@@ -9,6 +9,57 @@ function main(){
     }
 }
 
+function openDialog(x, y, type){
+    if (document.getElementsByTagName("dialog").length > 0){
+        document.getElementsByTagName("dialog")[0].remove();
+    }
+
+    const dialog = document.createElement("dialog");
+    let inner = "";
+    if (type === "addShiftFromRun"){
+        inner = ('<button autofocus id="closeButton">Cancel</button>'
+                 + "<p>Adding Shift from Run dialog</p>"
+                 + "</dialog>")
+    }
+    else if (type === "editShift"){
+        inner = ('<button autofocus id="closeButton">Cancel</button>'
+                 + "<p>Editing Shift</p>"
+                 + "</dialog>")
+    }
+    else if (type === "removeShift"){
+        inner = ('<button autofocus id="closeButton">Cancel</button>'
+                 + "<p>Removing Shift</p>"
+                 + "</dialog>")
+    }
+
+    dialog.innerHTML = inner;
+    dialog.classList.add("form-dialog");
+    dialog.style = `position: absolute; left: ${x}px; top: ${y}px; margin: 0`
+    document.body.appendChild(dialog);
+
+    const closeButton = document.getElementById("closeButton");
+    closeButton.addEventListener("click", () => {
+        dialog.close();
+        dialog.remove();
+    })
+    dialog.show();
+}
+
+document.addEventListener('click', function(e){
+    if (e.target.classList.contains("addShiftFromRun")){
+        console.log("addShiftFromRun");
+        openDialog(e.clientX, e.clientY, "addShiftFromRun")
+    }
+    else if (e.target.classList.contains("editShift")){
+        console.log("editShift");
+        openDialog(e.clientX, e.clientY, "editShift")
+    }
+    else if (e.target.classList.contains("removeShift")){
+        console.log("removeShift");
+        openDialog(e.clientX, e.clientY, "removeShift");
+    }
+})
+
 const waitLoad = setInterval(() => {
     if (document.getElementsByClassName("block") !== null){
         clearInterval(waitLoad);
