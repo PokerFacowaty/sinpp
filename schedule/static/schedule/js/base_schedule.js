5b361fa5d2a29ed5898f16324cb5f0be30e2b0ae
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-10-12 20:54:48 +0000

    Added getCookie

--- schedule/static/schedule/js/base_schedule.js
+++ schedule/static/schedule/js/base_schedule.js
@@ -45,6 +45,24 @@ function openDialog(x, y, type){
     dialog.show();
 }
 
+// https://docs.djangoproject.com/en/4.2/howto/csrf/#acquiring-the-token-if-csrf-use-sessions-and-csrf-cookie-httponly-are-false
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
 document.addEventListener('click', function(e){
     if (e.target.classList.contains("addShiftFromRun")){
         console.log("addShiftFromRun");
