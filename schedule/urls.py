cf407a12bd0c0dbf2bc8f015bd6353f83e556d93
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-10-14 18:08:22 +0000

    Line too long

--- schedule/urls.py
+++ schedule/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('upload_csv/', views.upload_csv, name="upload_csv"),
     path('add_event/', views.add_event, name="add_event"),
-    path('schedule/<int:event_id>/<int:room_id>/', views.schedule, name='schedule'),
+    path('schedule/<int:event_id>/<int:room_id>/', views.schedule,
+         name='schedule'),
     path('shift/<int:shift_id>/', views.shift),
     path('add_shift/', views.add_shift),
     path('', views.index, name='main')
