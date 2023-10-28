165eba64432dd8ff0b0176d70a8e9116f9c74e3a
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-10-28 00:15:03 +0000

    Added adding new staff members

---
+++ schedule/static/schedule/js/base_event.ts
@@ -0,0 +1,141 @@
+function main_(){
+    const getAllUsersBtn = document.getElementById("get-users-button");
+
+    getAllUsersBtn.addEventListener("click", async (e: MouseEvent) => {
+        eventCleanUp();
+        const users = await getAllUsers();
+        const dialog = createAddStaffDialog(e.pageX, e.pageY, users);
+        document.body.appendChild(dialog);
+        dialog.show();
+    });
+}
+
+function eventCleanUp(): void {
+    const dialog = document.getElementById(
+        "staff-dialog") as HTMLDialogElement;
+    if (dialog) dialog.remove();
+}
+
+async function getAllUsers(): Promise<[string[]]> {
+    const url = window.location.origin + '/all_usernames/';
+    const response = await fetch(url, {
+            method: "GET",
+            credentials: "same-origin",
+            headers: {
+                "X-Requested-With": "XMLHttpRequest",
+                "X-CSRFToken": CSRFTOKEN
+            }
+        })
+
+    const js = await response.json();
+    const users = await js['context'];
+    return JSON.parse(users);
+}
+
+function createAddStaffDialog(x: number, y: number,
+                              users: [string[]]): HTMLDialogElement {
+    const dialog = document.createElement('dialog');
+    dialog.id = 'staff-dialog';
+
+    // Clicking outside the dialog should close it...
+    document.body.addEventListener("click", () => {
+        eventCleanUp();
+        document.body.removeEventListener("click", () => {}, false);
+    })
+
+    // ...but not clicking on the dialog itself
+    dialog.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+    })
+
+    const clsBtn = document.createElement('button');
+    clsBtn.id = 'close-button';
+    clsBtn.innerHTML = 'Cancel';
+    clsBtn.addEventListener('click', () => {
+        eventCleanUp();
+    });
+    dialog.appendChild(clsBtn);
+
+    const inp = document.createElement('input');
+    inp.setAttribute("list", "datalist-add-users");
+    inp.type = 'text';
+    inp.id = 'username-input';
+    dialog.appendChild(inp);
+
+    const dtl = document.createElement('datalist');
+    dtl.id = "datalist-add-users";
+    dialog.appendChild(dtl);
+
+    for (const [id, username] of users){
+        const opt = document.createElement("option");
+        opt.value = username;
+        opt.dataset.userId = id;
+        dtl.appendChild(opt);
+    }
+
+    const sendBtn = document.createElement("button");
+    sendBtn.id = 'send-button';
+    sendBtn.innerHTML = 'Add';
+    sendBtn.addEventListener('click', sendStaffRequest);
+    dialog.appendChild(sendBtn);
+
+    dialog.setAttribute('style', `position: absolute; left: ${x}px;`
+                                  + `top: ${y}px; margin: 0`)
+    return dialog;
+};
+
+async function sendStaffRequest(){
+    const currentURL = window.location.href;
+    const URLsegments = new URL(currentURL).pathname.split('/');
+
+    // this takes care of possible trailing slashes making '' the last element
+    const eventId = URLsegments.pop() || URLsegments.pop()
+    const username = (document.getElementById(
+                      'username-input')as HTMLInputElement).value;
+    const url = window.location.origin + `/add_staff/${eventId}/`;
+
+    const response = await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "X-Requested-With": "XMLHttpRequest",
+            "X-CSRFToken": CSRFTOKEN
+        },
+        body: JSON.stringify({payload: username})
+    })
+
+    if (response.ok){
+        eventCleanUp();
+        addUserToStaffList(username);
+    }
+}
+
+function addUserToStaffList(username: string): void {
+    const staffList = document.getElementById('staff-list');
+    const newUser = document.createElement('li');
+    newUser.innerHTML = username;
+    staffList.appendChild(newUser);
+}
+
+function getEventCookie(name: string) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(
+                                                 name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const CSRFTOKEN: string = getEventCookie('csrftoken');
+
+window.onload = main_;
