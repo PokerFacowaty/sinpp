2c1e662e5f0bfc305ffd6fa6c8201e52a5704ede
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-11-05 19:33:24 +0000

    Newly replaced test now passes

--- schedule/tests/test_event.py
+++ schedule/tests/test_event.py
@@ -2,18 +2,20 @@ from django.test import TestCase
 from django.utils import timezone
 from schedule.models import Event
 from datetime import timedelta
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
+from django.test import Client
 
 
 class EventTestCase(TestCase):
     def setUp(self):
+        self.user = User.objects.create_user("notverysuper", "", "password1")
+        c = Client()
         start_date = timezone.now()
         end_date = timezone.now() + timedelta(days=1)
-        ev = Event.create(NAME="GSPS 2026",
-                          SLUG="GSPS26",
-                          START_DATE_TIME=start_date,
-                          END_DATE_TIME=end_date)
-        ev.save()
+        c.login(username="notverysuper", password="password1")
+        c.post("/add_event/", {"NAME": "GSPS 2026", "SLUG": "GSPS26",
+                               "START_DATE_TIME": start_date,
+                               "END_DATE_TIME": end_date})
 
     def test_proper_group_was_made(self):
         self.assertTrue(Group.objects.filter(name="GSPS26 Staff"))
