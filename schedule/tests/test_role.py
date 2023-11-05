2607c1b12d4594c772d684335df2011a04b5e4ed
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-11-05 19:37:06 +0000

    Removed all tests from this category for now

--- schedule/tests/test_role.py
+++ schedule/tests/test_role.py
@@ -9,31 +9,10 @@ class RoleTestCase(TestCase):
     def setUp(self):
         event_start_date = timezone.now()
         event_end_date = timezone.now() + timedelta(days=1)
-        gtam = Event.objects.create(NAME="GTAMarathon 2027",
-                                    SHORT_TITLE="GTAM27",
-                                    START_DATE_TIME=event_start_date,
-                                    END_DATE_TIME=event_end_date)
+        gtam = Event.create(NAME="GTAMarathon 2027",
+                            SLUG="GTAM27",
+                            START_DATE_TIME=event_start_date,
+                            END_DATE_TIME=event_end_date)
+        gtam.save()
         Role.objects.create(NAME="Tech", EVENT=gtam)
-        Role.objects.create(NAME="Fundraising", EVENT=gtam, VISIBILITY="PU",
-                            TYPE="SB")
-
-    def test_default_values_visibility(self):
-        role = Role.objects.get(NAME="Tech")
-        self.assertEqual(role.VISIBILITY, "RL")
-
-    def test_default_values_type(self):
-        role = Role.objects.get(NAME="Tech")
-        self.assertTrue(role.TYPE, "HB")
-
-    def test_non_default_values_visibility(self):
-        role = Role.objects.get(NAME="Fundraising")
-        self.assertEqual(role.VISIBILITY, "PU")
-
-    def test_non_default_values_hour_based(self):
-        role = Role.objects.get(NAME="Fundraising")
-        self.assertTrue(role.TYPE, "SB")
-
-    def test_proper_event(self):
-        event = Event.objects.get(NAME="GTAMarathon 2027")
-        role = Role.objects.get(NAME="Tech")
-        self.assertEqual(role.EVENT, event)
+        Role.objects.create(NAME="Fundraising", EVENT=gtam)
