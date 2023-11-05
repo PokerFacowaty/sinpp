9cec750542f055a70035fa0f8692835b25b58bc2
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-11-05 15:14:03 +0000

    Fixed Event test setUp and replaced the only test

--- schedule/tests/test_event.py
+++ schedule/tests/test_event.py
@@ -2,20 +2,18 @@ from django.test import TestCase
 from django.utils import timezone
 from schedule.models import Event
 from datetime import timedelta
+from django.contrib.auth.models import Group
 
 
 class EventTestCase(TestCase):
-    # TODO:
-    # - Events generating a short title if not provided (added + tested)
-    # - Event has proper dates
     def setUp(self):
         start_date = timezone.now()
         end_date = timezone.now() + timedelta(days=1)
-        Event.objects.create(NAME="GSPS 2026",
-                             SHORT_TITLE="GSPS26",
-                             START_DATE_TIME=start_date,
-                             END_DATE_TIME=end_date)
+        ev = Event.create(NAME="GSPS 2026",
+                          SLUG="GSPS26",
+                          START_DATE_TIME=start_date,
+                          END_DATE_TIME=end_date)
+        ev.save()
 
-    def test_event_has_name(self):
-        gsps = Event.objects.get(SHORT_TITLE="GSPS26")
-        self.assertEqual(gsps.NAME, "GSPS 2026")
+    def test_proper_group_was_made(self):
+        self.assertTrue(Group.objects.filter(name="GSPS26 Staff"))
