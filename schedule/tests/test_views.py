b3ce687fc1bb6d10257fc539e425dce1a865b7ec
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-11-06 02:47:20 +0000

    Added a RequestFactory to TestEditEvent

--- schedule/tests/test_views.py
+++ schedule/tests/test_views.py
@@ -76,8 +76,10 @@ class TestEvent(TestCase):
 class TestEditEvent(TestCase):
 
     def setUp(self):
-        staff_user = User.objects.create_user("notsosuper", "", "mypassword")
-        User.objects.create_user("regularjoe", "", "password123")
+        self.staff_user = User.objects.create_user("notsosuper",
+                                                   "", "mypassword")
+        self.non_staff_user = User.objects.create_user("regularjoe",
+                                                       "", "password123")
 
         start = datetime(year=2018, month=6, day=21, hour=11,
                          tzinfo=timezone.utc)
@@ -87,12 +89,14 @@ class TestEditEvent(TestCase):
                           START_DATE_TIME=start,
                           END_DATE_TIME=end)
         ev.save()
-        staff_user.groups.add(ev.STAFF)
+        self.staff_user.groups.add(ev.STAFF)
 
         self.staff_c = Client()
         self.staff_c.login(username="notsosuper", password="mypassword")
         self.non_staff_c = Client()
         self.non_staff_c.login(username="regularjoe", password="password123")
+
+        self.factory = RequestFactory()
 
     def test_edit_event_get_200(self):
         response = self.staff_c.get("/edit_event/GTAM27/")
