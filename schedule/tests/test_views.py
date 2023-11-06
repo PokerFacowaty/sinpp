a095cf15dcfb24723a0ff5f02d365d3f548338e6
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-11-06 00:41:57 +0000

    Added non-staff test

--- schedule/tests/test_views.py
+++ schedule/tests/test_views.py
@@ -17,7 +17,9 @@ class TestAddEvent(TestCase):
 class TestEvent(TestCase):
 
     def setUp(self):
-        user = User.objects.create_user("notsosuper", "", "mypassword")
+        staff_user = User.objects.create_user("notsosuper", "", "mypassword")
+        User.objects.create_user("regularjoe", "", "password123")
+
         start = datetime(year=2018, month=6, day=21, hour=11,
                          tzinfo=timezone.utc)
         end = start + timedelta(days=1)
@@ -26,19 +28,26 @@ class TestEvent(TestCase):
                           START_DATE_TIME=start,
                           END_DATE_TIME=end)
         ev.save()
-        user.groups.add(ev.STAFF)
-        self.c = Client()
-        self.c.login(username="notsosuper", password="mypassword")
+        staff_user.groups.add(ev.STAFF)
+
+        self.staff_c = Client()
+        self.staff_c.login(username="notsosuper", password="mypassword")
+        self.non_staff_c = Client()
+        self.non_staff_c.login(username="regularjoe", password="password123")
 
     def test_event_200(self):
-        response = self.c.get("/event/GTAM27/")
+        response = self.staff_c.get("/event/GTAM27/")
         self.assertEqual(response.status_code, 200)
 
     def test_event_template(self):
-        response = self.c.get("/event/GTAM27/")
+        response = self.staff_c.get("/event/GTAM27/")
         self.assertIn("schedule/base_event.html",
                       [x.name for x in response.templates])
 
     def test_event_doesnt_exit(self):
-        response = self.c.get("/event/GDQ29/")
+        response = self.staff_c.get("/event/GDQ29/")
         self.assertEqual(response.status_code, 404)
+
+    def test_event_user_not_staff(self):
+        response = self.non_staff_c.get("/event/GTAM27/")
+        self.assertEqual(response.status_code, 403)
