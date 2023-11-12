94497059ab360b58310f53a95bc8afa067a5611c
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-11-12 09:06:34 +0000

    Added availability tests

--- schedule/tests/test_models.py
+++ schedule/tests/test_models.py
@@ -218,10 +218,10 @@ class TestPerson(TestCase):
                                END_DATE_TIME=ev_end)
         self.ev.save()
 
-        tech = Role.objects.create(NAME="Tech",
-                                   EVENT=self.ev)
+        self.tech = Role.objects.create(NAME="Tech",
+                                        EVENT=self.ev)
         self.prsn = Person.objects.create(NICKNAME="MyPerson")
-        self.prsn.ROLES.set([tech])
+        self.prsn.ROLES.set([self.tech])
 
         # Available for the tech shift
         avail1_start = ev_start
@@ -232,10 +232,20 @@ class TestPerson(TestCase):
                                                 START_DATE_TIME=avail1_start,
                                                 END_DATE_TIME=avail1_end)
         self.tech_shift = Shift.objects.create(
-                          ROLE=tech, EVENT=self.ev,
+                          ROLE=self.tech, EVENT=self.ev,
                           START_DATE_TIME=ev_start + timedelta(hours=1),
                           END_DATE_TIME=ev_start + timedelta(hours=2))
         self.tech_shift.VOLUNTEERS.set([self.prsn])
+
+    def test_if_available(self):
+        self.assertTrue(self.prsn.is_available(self.tech_shift.START_DATE_TIME,
+                                               self.tech_shift.END_DATE_TIME,
+                                               self.tech))
+
+    def test_if_not_available(self):
+        st = self.ev.END_DATE_TIME - timedelta(hours=2)
+        end = self.ev.END_DATE_TIME - timedelta(hours=1)
+        self.assertFalse(self.prsn.is_available(st, end, self.tech))
 
     def test_if_busy(self):
         start = (Event.objects.get(SLUG="GDQ2").START_DATE_TIME
