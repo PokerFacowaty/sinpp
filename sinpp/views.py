2e0fc1ff075d16e0ec58c154cb01697f999f5dd6
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-11-12 10:06:34 +0000

    Updated so that the test passes

--- sinpp/views.py
+++ sinpp/views.py
@@ -5,20 +5,23 @@ from schedule.views import index
 from schedule.models import Event, Room, Role
 from django.db.models import Q
 from django.contrib.auth.models import User, Group
+from django.http import HttpResponseBadRequest
 
 
 @login_required
 def user_profile(request):
-    usr = request.user
-    usr_groups = usr.groups.all()
-    usr_events = Event.objects.filter(Q(STAFF__in=usr_groups))
-    for ev in usr_events:
-        # lowercase to differentiate from DB values
-        ev.roles = Role.objects.filter(EVENT=ev)
-        ev.staff = User.objects.filter(groups__name=ev.STAFF)
-        ev.rooms = Room.objects.filter(EVENT=ev)
-    return render(request, 'registration/profile.html',
-                  {'events': usr_events})
+    if request.method == "GET":
+        usr = request.user
+        usr_groups = usr.groups.all()
+        usr_events = Event.objects.filter(Q(STAFF__in=usr_groups))
+        for ev in usr_events:
+            # lowercase to differentiate from DB values
+            ev.roles = Role.objects.filter(EVENT=ev)
+            ev.staff = User.objects.filter(groups__name=ev.STAFF)
+            ev.rooms = Room.objects.filter(EVENT=ev)
+        return render(request, 'registration/profile.html',
+                      {'events': usr_events})
+    return HttpResponseBadRequest()
 
 
 def register_account(request):
